c48264648af8c73c0493eafab0a461b18aca79ec
Gleb_Protasov <94696376+HlibProtasov@users.noreply.github.com>
2025-05-30 19:20:15 +0300

    updated task

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,23 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {AccessControl} from './AccessControl.sol';
+import {AccessControlUpgradeable} from '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import {UUPSUpgradeable} from '@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol';
 import {ERC20Upgradeable} from '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+
+bytes32 constant MINTER_ROLE = keccak256("MINTER_ROLE");
+bytes32 constant BURNER_ROLE = keccak256("BURNER_ROLE");
+bytes32 constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+
+bytes32 constant FREEZE_ROLE = keccak256("FREEZE_ROLE");
+
+bytes32 constant StorageAddress = keccak256("FREEZE_ROLE");
 
 
 /// @title Upgradeable ERC20 Token with Admin-Controlled Minting and Burning
 /// @notice This ERC20 token allows only administrators to mint, burn, and upgrade the contract
-contract ERC20 is ERC20Upgradeable, UUPSUpgradeable {
-    AccessControl private _accessControl;
+contract ERC20 is ERC20Upgradeable, UUPSUpgradeable, AccessControlUpgradeable {
+  
+  /// keccak256('erc20.frozen.storage')
+  bytes32 private constant ERC20FrozenStorage = 0x7c4e55af7ca6aff6469db4449710dda4adcf6ad6ebbdeaf3d2080aa1ba020f61;
+ 
+   struct FrozeData {
+        mapping(address => uint) frozenBalances;
+    }
+
+
+ function _getERC20FrozenStorage() private pure returns (FrozeData storage s) {
+        assembly {
+            s.slot := ERC20FrozenStorage
+        }
+    }
+
+
+  
+    function freezeBalance(address account, uint amount) public onlyRole(FREEZE_ROLE) {
+        require(balanceOf(account) >= amount, "Insufficient balance to freeze");
+        FrozeData storage frozenData = _getERC20FrozenStorage();
+        frozenData.frozenBalances[account] += amount;
+    }
+
+   function _update(address from, address to, uint256 value) internal override {
+        FrozeData storage frozenData = _getERC20FrozenStorage();
+        require(balanceOf(from) - frozenData.frozenBalances[from] >= value, "Insufficient balance after freeze");
+        super._update(from, to, value);
+    }
+
+    function withdrawFrozenBalance(uint amount, address from, address to) public onlyRole(WITHDRAW_ROLE) {
+        FrozeData storage frozenData = _getERC20FrozenStorage();
+        require(frozenData.frozenBalances[from] >= amount, "Insufficient frozen balance");
+        frozenData.frozenBalances[msg.sender] -= amount;
+        _transfer(from, to, amount);
+    }
 
 
     /// @notice Mints new tokens to a specified address
     /// @dev Only callable by an admin as defined in the AccessControl contract
     /// @param to The address that will receive the newly minted tokens
     /// @param amount The number of tokens to mint
-    function mint(address to, uint256 amount) public {
-        require(_accessControl.isAdmin(msg.sender), 'ERC20: Only admin can mint');
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
@@ -26,15 +67,13 @@ contract ERC20 is ERC20Upgradeable, UUPSUpgradeable {
     /// @dev Only callable by an admin as defined in the AccessControl contract
     /// @param from The address from which tokens will be burned
     /// @param amount The number of tokens to burn
-    function burn(address from, uint256 amount) public {
-        require(_accessControl.isAdmin(msg.sender), 'ERC20: Only admin can burn');
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
         _burn(from, amount);
     }
 
     /// @notice Authorizes contract upgrades
     /// @dev Only callable by an admin
-    function _authorizeUpgrade(address /*newImplementation*/) internal override view {
-        require(_accessControl.isAdmin(msg.sender), 'ERC20: Only admin can upgrade');
+    function _authorizeUpgrade(address /*newImplementation*/) internal override view  onlyRole(DEFAULT_ADMIN_ROLE) {
     }
 
 
@@ -42,10 +81,9 @@ contract ERC20 is ERC20Upgradeable, UUPSUpgradeable {
     /// @dev This function replaces the constructor for upgradeable contracts
     /// @param name The name of the token
     /// @param symbol The symbol of the token
-    /// @param accessControl The address of the deployed AccessControl contract
-    function initialize(string memory name, string memory symbol, address accessControl) public initializer {
+    function initialize(string memory name, string memory symbol) public initializer {
         __ERC20_init(name, symbol);
-        _accessControl = AccessControl(accessControl);
+        __AccessControl_init();
     }
 
 }
